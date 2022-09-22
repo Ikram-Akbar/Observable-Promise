ccd920ae790c7c062207c914bf566eabdb7ecbfd
Ikram Akbar <ikramakbar71@gmail.com>
2022-09-22 16:19:59 +0600

    routind added

--- src/app/promise/promise.component.ts
+++ src/app/promise/promise.component.ts
@@ -3,13 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-promise',
   templateUrl: './promise.component.html',
-  styleUrls: ['./promise.component.scss']
+  styleUrls: ['./promise.component.scss'],
 })
 export class PromiseComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  dellAvailable() {
+    return true;
+  }
+  hpAvailable() {
+    return false;
   }
 
+  ngOnInit(): void {
+    // make a promise like
+    const byeLaptop = new Promise((resolve, reject) => {
+      /* 1st case : General case
+       resolve("promise is resolved")
+       reject('promise is rejected ');*/
+
+      // 2nd case: if else
+      if (this.dellAvailable()) {
+         setTimeout(() => {
+          resolve('Dell is Purchased');
+        }, 5000);
+      } else if (this.hpAvailable()) {
+        setTimeout(() => {
+          resolve('HP is Purchased ');
+        }, 5000);
+      } else {
+        reject('sorry no product is available ');
+      }
+    });
+    byeLaptop
+      .then((res) => console.log(`the result is : ${res}`))
+      .catch((res) => {
+        console.log(`the result is : ${res}`);
+      });
+  }
 }
