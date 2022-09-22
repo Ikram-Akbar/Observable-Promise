b7e702fd82748ea8d9fb82dcc5db9cf1cf45057f
Ikram Akbar <ikramakbar71@gmail.com>
2022-09-22 15:44:38 +0600

    routind added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PromiseComponent } from './promise/promise.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:"promise",pathMatch:"full"},
+  { path: 'promise', component: PromiseComponent },
+  {path:"rxjs", component:RxjsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
